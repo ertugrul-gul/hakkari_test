9fc3e59f8eea1a650b40befa5b3747ebe3a0334a
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-02-07 00:03:05 +0300

    Create first.py

---
+++ first.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 22:35:09 2025
+
+@author: ertugrulgul
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+veri = "data_H/hakkari.csv"
+veri = pd.read_csv(veri)
+print(veri)
