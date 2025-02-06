64fa1b77339e6c6bbf1914d6b5269c2d9e295929
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-02-07 00:03:25 +0300

    Delete test_tensorflow.py

--- test_tensorflow.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-# GPU'ları kontrol et
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    print(f"GPU'lar başarıyla algılandı: {gpus}")
-else:
-    print("GPU algılanamadı, sadece CPU kullanılacak.")
