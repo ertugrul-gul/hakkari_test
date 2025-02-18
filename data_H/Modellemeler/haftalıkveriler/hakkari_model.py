a4938599fb0f918b486636f2f8ff4d54cfd92ddb
ertugul0848 <114884594+ertugul0848@users.noreply.github.com>
2025-02-19 01:38:35 +0300

    few

--- data_H/Modellemeler/haftalıkveriler/hakkari_model.py
+++ data_H/Modellemeler/haftalıkveriler/hakkari_model.py
@@ -1,8 +1,25 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("hakkari_end.csv")
-print (df.info())
-print (df.head())
-print(df)
+
+
+df = pd.read_csv("hakkari_0.csv")
+
+plt.figure(figsize=(12,5))
+plt.plot(df.index, df["t2m"], label="Sıcaklık")
+plt.title("Zaman Serisi Grafiği")
+plt.xlabel("Tarih")
+plt.ylabel("Sıcaklık")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(12,5))
+plt.plot(df.index, df["sp"], label="Basınç")
+plt.title("Zaman Serisi Grafiği")
+plt.xlabel("Tarih")
+plt.ylabel("Basınç")
+plt.legend()
+plt.show()
+
 
 
